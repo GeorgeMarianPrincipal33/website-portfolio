3516bf0a4c7724dbe14ac83eb98e252982867936
AzureAD\GeorgeMarian <george.marian@principal33.com>
2021-09-21 15:42:27 +0300

    restore project

---
+++ backend/Webportfolio/Webportfolio/Services/CVService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webportfolio.Data;
+using Webportfolio.Models;
+
+namespace Webportfolio.Services
+{
+    public class CVService : ICVService
+    {
+
+        private readonly ApplicationDbContext dbContext;
+
+        public CVService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public IEnumerable<CV> GetCVs()
+        {
+            return dbContext.CVs.ToArray();
+        }
+    }
+}
---
+++ backend/Webportfolio/Webportfolio/Services/ICVService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webportfolio.Models;
+
+namespace Webportfolio.Services
+{
+    public interface ICVService
+    {
+        IEnumerable<CV> GetCVs();
+
+    }
+}
