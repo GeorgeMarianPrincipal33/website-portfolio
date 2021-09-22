fb4f8461904105235ff44f64adaabfb190218fc5
AzureAD\GeorgeMarian <george.marian@principal33.com>
2021-09-22 13:26:11 +0300

    ensure that users are created

--- backend/Webportfolio/Webportfolio/Startup.cs
+++ backend/Webportfolio/Webportfolio/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +30,11 @@ namespace Webportfolio
             services.AddControllersWithViews();
             services.AddScoped<ICVService, CVService>();
             services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("PostgresConnection")));
+
+            //services.AddDefaultIdentity<ApplicationUser>()
+            //        .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            EnsureUsersCreated(services).Wait();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +63,40 @@ namespace Webportfolio
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+        private static async Task EnsureUsersCreated(IServiceCollection services)
+        {
+            var serviceProvider = services.BuildServiceProvider();
+            var userManager = serviceProvider.GetService<UserManager<IdentityUser>>();
+
+            var user1 = await EnsureUserCreated(userManager, "george.marian@principal33.com", "Parola1@");
+            var user2 = await EnsureUserCreated(userManager, "dragos.duma@principal33.com", "Parola2@");
+            var user3 = await EnsureUserCreated(userManager, "leon.campean@principal33.com", "Parola3@");
+            var user4 = await EnsureUserCreated(userManager, "catalin.briscan@principal33.com", "Parola4@");
+
+            //var users = await userManager.Users.ToListAsync();
+            //Console.WriteLine($"There are {users.Count} users now.");
+        }
+
+        private static async Task<IdentityUser> EnsureUserCreated(UserManager<IdentityUser> userManager, string name, string password)
+        {
+            var adminUser = await userManager.FindByNameAsync(name);
+            if (adminUser == null)
+            {
+                await userManager.CreateAsync(new IdentityUser(name));
+                adminUser = await userManager.FindByNameAsync(name);
+                var tokenChangePassword = await userManager.GeneratePasswordResetTokenAsync(adminUser);
+
+                var result = await userManager.ResetPasswordAsync(adminUser, tokenChangePassword, password);
+
+                if (!adminUser.EmailConfirmed)
+                {
+                    var token = await userManager.GenerateEmailConfirmationTokenAsync(adminUser);
+                    await userManager.ConfirmEmailAsync(adminUser, token);
+                }
+            }
+
+            return adminUser;
         }
     }
 }
