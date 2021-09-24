e591ca91a1197c66cc693ba371553c82ab32547e
AzureAD\GeorgeMarian <george.marian@principal33.com>
2021-09-24 14:04:33 +0300

    api calls for backend

--- frontend/website-portfolio/src/scripts/connection.js
+++ frontend/website-portfolio/src/scripts/connection.js
@@ -1,16 +1,15 @@
-import {BACKEND} from './variables'
+import {BACKEND_URL} from './variables'
 
 var axios = require("axios");
 
 export async function getCVs() {
-    const url = BACKEND + "CV/GetCVs";
+    const url = BACKEND_URL + "CV/GetCVs";
   
     return axios.get(url);
 }
 
 export async function login(username, password){
-    const url = BACKEND + `User/LoginFunction?userName=${username}&password=${password}`;
-    console.log('connection, ', url)
-  
-    // return axios.get(url);
+    const url = BACKEND_URL + `/Users/LoginFunction?userName=${username}&password=${password}`;
+    
+    return axios.get(url);
 }
