902e5ae260b5533c217e76aa59832006a0c5331f
AzureAD\DragosDuma <dragos.duma@gmail.com>
2021-09-24 12:26:59 +0300

    Created authentification request method

---
+++ backend/Webportfolio/Webportfolio/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Webportfolio.Data;
+
+
+namespace Webportfolio.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+        private readonly UserManager<IdentityUser> userManager;
+        public UsersController(ApplicationDbContext dbContext, UserManager<IdentityUser> userManager)
+        {
+            this.dbContext = dbContext;
+            this.userManager = userManager;
+        }
+        [HttpGet]
+        public string LoginFunction(string userName, string password)
+        {
+            IdentityUser identityUser = dbContext.Users.FirstOrDefault(e => e.UserName == userName);
+            if (identityUser != null)
+            {
+                var verificationResult = userManager.PasswordHasher.VerifyHashedPassword(identityUser, identityUser.PasswordHash, password);
+                if (verificationResult == PasswordVerificationResult.Success)
+                {
+                    return identityUser.Id;
+                }
+            }
+            return null;
+        }
+    }
+}
