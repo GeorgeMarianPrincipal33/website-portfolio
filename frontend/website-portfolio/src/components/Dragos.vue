8b967516d8526601f6cf816f24317178b5188032
BriscanCatalinOnisim <catalin.brscan@principal33.com>
2021-09-26 14:11:41 +0300

    creating the other participants

---
+++ frontend/website-portfolio/src/components/Dragos.vue
@@ -0,0 +1,58 @@
+<template>
+<NavbarComponent></NavbarComponent>
+<DetailsComponent></DetailsComponent>
+<AboutComponent v-if="getData" :description="description"></AboutComponent>
+<CredentialsComponent v-if="getData" :certifications="certifications" :placesOfWork="placesOfWork" :skills="skills"></CredentialsComponent>
+<FooterComponent v-if="getData" :contactDetails="contactDetails"></FooterComponent>
+</template>
+
+<script>
+import FooterComponent from './FooterComponent.vue'
+import NavbarComponent from './NavbarComponent.vue'
+import DetailsComponent from './DetailsComponent.vue'
+import AboutComponent from './AboutComponent.vue'
+import CredentialsComponent from './CredentialsComponent.vue'
+
+import { getCV } from "../scripts/connection.js";
+
+export default {
+    name: 'Dragos',
+    components: {
+        NavbarComponent,
+        FooterComponent,
+        DetailsComponent,
+        AboutComponent,
+        CredentialsComponent,
+    },
+
+    data(){
+        return {
+            description:'',
+            contactDetails: '',
+            certifications: '',
+            placesOfWork: '',
+            skills: '',
+            getData: false,
+        }
+    },
+
+    mounted() {
+        getCV(5).then(response => {
+            var result = response.data
+            this.description = result.description 
+            this.contactDetails = result.contactDetails 
+            this.certifications = result.certifications 
+            this.placesOfWork = result.placesOfWork 
+            this.skills = result.skills 
+
+            console.log(result)
+            this.getData = true
+        })
+    }
+
+}
+</script>
+
+<style>
+
+</style>
